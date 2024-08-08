e4f59b2e4db85c11a8ab2d09fd54f998410caad0
Yuvraj3k <Yuvraj3k@users.noreply.github.com>
2024-08-08 12:34:21 -0400

    api updated

--- apod_api.py
+++ apod_api.py
@@ -8,12 +8,11 @@ from datetime import date
 from sys import argv
 import image_lib
 import os
-
-NASA_API_KEY = 'GPptte59834VjWh13VsQRIvsr2a1mQJwhuz9RFZA'
+NASA_API_KEY = ''  
 APOD_URL = "https://api.nasa.gov/planetary/apod"
 
+
 def main():
-    #  Add code to test the functions in this module
     import sys
     num_params = len(sys.argv) - 1
     import sys
@@ -30,56 +29,31 @@ def main():
     return
 
 def get_apod_info(apod_date):
-    """Gets information from the NASA API for the Astronomy 
-    Picture of the Day (APOD) from a specified date.
-
-    Args:
-        apod_date (date): APOD date (Can also be a string formatted as YYYY-MM-DD)
-
-    Returns:
-        dict: Dictionary of APOD info, if successful. None if unsuccessful
-    """
-    # TODO: Complete the function body
-    pod_params = {
+    apod_params = {
         'api_key': NASA_API_KEY,
         'date': apod_date,
         'thumbs': True
     }
 
-    # Hint: The APOD API uses query string parameters: https://requests.readthedocs.io/en/latest/user/quickstart/#passing-parameters-in-urls
-    # Hint: Set the 'thumbs' parameter to True so the info returned for video APODs will include URL of the video thumbnail image 
+   
     print(f'Getting {apod_date} APOD information from NASA...', end='')
     resp_msg = requests.get(APOD_URL, params=apod_params)
 
     
     if resp_msg.status_code == requests.codes.ok:
         print('success')        
-        apod_info_dict = resp_msg.json() # Convert data to dictionary 
+        apod_info_dict = resp_msg.json() 
         return apod_info_dict
     else:
         print('failure')
         print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
         return 'failure'
 
-
 def get_apod_image_url(apod_info_dict):
-    """Gets the URL of the APOD image from the dictionary of APOD information.
-
-    If the APOD is an image, gets the URL of the high definition image.
-    If the APOD is a video, gets the URL of the video thumbnail.
-
-    Args:
-        apod_info_dict (dict): Dictionary of APOD info from API
-
-    Returns:
-        str: APOD image URL
-    """
-    # TODO: Complete the function body
     if apod_info_dict['media_type'] == 'image':
         return apod_info_dict['hdurl']
-    elif apod_info_dict['media_type'] == 'video': 
+    elif apod_info_dict['media_type'] == 'video':        
         return apod_info_dict['thumbnail_url']
-    # Hint: The APOD info dictionary includes a key named 'media_type' that indicates whether the APOD is an image or video
 
 def get_apod_date():
     num_params = len(sys.argv) - 1
@@ -92,7 +66,7 @@ def get_apod_date():
             print(f'Error: Invalid date format; {err}')
             sys.exit('Script execution aborted')
 
-        
+       
 
         MIN_APOD_DATE = date.fromisoformat("1995-06-16")  
         print(MIN_APOD_DATE, "MIN_APOD_DATE")
@@ -106,7 +80,9 @@ def get_apod_date():
         
         apod_date = date.today()  
 
-    return apod_date  
+    return apod_date
+
 
 if __name__ == '__main__':
+
     main()
